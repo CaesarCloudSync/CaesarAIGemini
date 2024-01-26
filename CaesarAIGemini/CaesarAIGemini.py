67760483d368ea4739040fdb8ce645446d62a1e4
amari <amari.lawal@gmail.com>
2024-01-26 19:30:02 +0000

    Gemini send message event stream works, history and image

--- CaesarAIGemini/CaesarAIGemini.py
+++ CaesarAIGemini/CaesarAIGemini.py
@@ -1,7 +1,10 @@
 import os
+import io
 import base64
+import json
 import google.generativeai as genai
 from dotenv import load_dotenv
+from PIL import Image
 load_dotenv("CaesarAIGemini/.env")
 class CaesarAIGemini:
   def __init__(self) -> None:
@@ -9,8 +12,20 @@ class CaesarAIGemini:
 
     self.model = genai.GenerativeModel('gemini-pro')
     self.chat = self.model.start_chat(history=[])
+    self.vision_model = genai.GenerativeModel('gemini-pro-vision')
   def send_message(self,message):
     response = self.chat.send_message(message, stream=True)
     for chunk in response:
       yield chunk.text
+  def get_history(self):
+   for message in self.chat.history:
+     yield json.dumps({message.role:message.parts[0].text})
+  def describe_image(self,image_content):
+    image_stream = io.BytesIO(image_content)
+    image_stream.seek(0)
+    img = Image.open(image_stream)
+    response = self.vision_model.generate_content(img)
+    return response.text
+    
+
      
--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from fastapi.responses import StreamingResponse,FileResponse,Response
 from typing import Dict,List,Any,Union
 from fastapi.middleware.cors import CORSMiddleware
 from CaesarAIGemini.CaesarAIGemini import CaesarAIGemini
+
+
+
 load_dotenv(".env")
 app = FastAPI()
 app.add_middleware(
@@ -43,6 +46,30 @@ async def sendmessage(message:str):
         except Exception as ex:
             return {"error":f"{type(ex)},{ex}"}
 
+@app.get('/gethistory')# GET # allow all origins all methods.
+async def gethistory():
+        try:
+            if len(caesargemini.chat.history) == 0:
+                 return {"message":"no previous history"}
+            else:
+                result = caesargemini.get_history()
+                #return {"history":result}
+                return StreamingResponse(result,media_type="application/stream+json")
+  
+        except Exception as ex:
+            return {"error":f"{type(ex)},{ex}"}
+
+@app.post('/sendvision')# GET # allow all origins all methods.
+async def sendvision(file: UploadFile = File(...)):
+        try:
+            img_content =  await file.read()
+    
+            result = caesargemini.describe_image(img_content)
+            return {"output_text":result}
+     
+  
+        except Exception as ex:
+            return {"error":f"{type(ex)},{ex}"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app",port=8080,log_level="info")
