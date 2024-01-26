e4bb7b55cd5bb516de6090f669ead513a5152a3f
amari <amari.lawal@gmail.com>
2024-01-26 18:23:27 +0000

    Gemini send message event stream works

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
-git push origin 
+git push origin -u main:main
 docker build -t palondomus/caesaraigemini:latest .
 docker push palondomus/caesaraigemini:latest
 docker run -it -p 8080:8080 palondomus/caesaraigemini:latest
