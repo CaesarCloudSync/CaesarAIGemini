4fa043497cbdf85459869867cc66c4c08e02dc3d
amari <amari.lawal@gmail.com>
2025-03-07 11:06:51 +0000

    change from pro to 1.5 pro

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "$1"
 git push origin -u main:main
-docker build -t palondomus/caesaraigemini:newest .
-docker push palondomus/caesaraigemini:newest
-docker run -it -p 8080:8080 palondomus/caesaraigemini:newest
+docker build -t palondomus/caesaraigemini:finest .
+docker push palondomus/caesaraigemini:finest
+docker run -it -p 8080:8080 palondomus/caesaraigemini:finest
